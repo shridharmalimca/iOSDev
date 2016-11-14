8aa7ded1d9888e4342eef394878def747310da80
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2016-11-14 15:48:47 +0530

    Added alert functionality

--- AlertController/AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/AlertController/ViewController.swift
@@ -15,6 +15,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func simpleAlert(_ sender: Any) {
+        let alertController = UIAlertController(title:"", message: "This is simple alert", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        })
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func alertWithTitle(_ sender: Any) {
+        let alertController = UIAlertController(title:"Alert Title", message: "This is simple alert", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        })
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    @IBAction func alertWithTwoButtons(_ sender: Any) {
+        let alertController = UIAlertController(title:"Alert Title", message: "This is simple alert", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { (action: UIAlertAction) in
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
