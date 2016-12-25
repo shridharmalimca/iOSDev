f3580f265af85cb8ff6bb00339a9f8b6d031d182
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2016-12-25 15:02:34 +0530

    TabBarController added

---
+++ iOS/Swift 3.0/TabBarController/TabBarController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabBarController
+//
+//  Created by Shridhar Mali on 12/25/16.
+//  Copyright © 2016 TIS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
