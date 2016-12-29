06552e614e2d1120ef2608a06b4b5f3d65072fee
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2016-12-29 13:46:07 +0530

    Layout programtically

---
+++ iOS/Components/ShoppingCart/ShoppingCart/Addition.swift
@@ -0,0 +1,13 @@
+//
+//  Addition.swift
+//  
+//
+//  Created by Shridhar Mali on 12/28/16.
+//
+//
+
+import UIKit
+
+class Addition: NSObject {
+
+}
---
+++ iOS/Swift 3.0/CoreData/CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Shridhar Mali on 12/28/16.
+//  Copyright © 2016 TIS. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
