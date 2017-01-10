eed9c36406dcf03f16e850cb7c8bbb8f005a5f32
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2017-01-10 15:26:24 +0530

    MapView Related changes

--- iOS/Swift 3.0/MapView/MapView/ViewController.swift
+++ iOS/Swift 3.0/MapView/MapView/ViewController.swift
@@ -9,33 +9,44 @@
 import UIKit
 import MapKit
 import CoreLocation
-class ViewController: UIViewController, CLLocationManagerDelegate {
-
+class ViewController: UIViewController {
+    
     @IBOutlet weak var mapView: MKMapView!
+    let objLocationManager = LocationManagerHelper.sharedInstance
     override func viewDidLoad() {
         super.viewDidLoad()
-        let locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestAlwaysAuthorization()
-        // 1 Create a location
-        let location = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(self.refreshMap))
+        objLocationManager.updateUserLocation()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addAnnotation()
+    }
+    func refreshMap() {
+        addAnnotation()
+    }
+    func addAnnotation() {
+        print(objLocationManager.userLocation.coordinate.latitude)
+        print(objLocationManager.userLocation.coordinate.longitude)
         
-        
-        // 2 Create span 
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
+        // 2 Create span
+        var span = MKCoordinateSpanMake(0.05, 0.05)
+        span.latitudeDelta = 0.02
+        span.longitudeDelta = 0.02
+        let region = MKCoordinateRegion(center: objLocationManager.userLocation.coordinate, span: span)
         mapView.setRegion(region, animated: true)
         
         
-        // 3 Add Annotation on map 
+        // 3 Add Annotation on map
+        let annotations = mapView.annotations
+        mapView.removeAnnotations(annotations)
         let annotation = MKPointAnnotation()
-        annotation.coordinate = location
+        annotation.coordinate = objLocationManager.userLocation.coordinate
         annotation.title = "Title"
         annotation.subtitle = "SubTitle"
         mapView.addAnnotation(annotation)
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,15 +54,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print("User current location is:\(location)")
-        }
-    }
     
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Failed to get the current location of user \(error.localizedDescription)")
-    }
 
 }
 
