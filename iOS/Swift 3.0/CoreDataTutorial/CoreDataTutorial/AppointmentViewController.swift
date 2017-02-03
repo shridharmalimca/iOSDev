20380985a68ba82350ef26f9a4c3808980766c5d
Shridar Ananda <Shridar.Ananda@altimetrik.com>
2017-02-03 11:47:56 +0530

    Core data tutorial added

---
+++ iOS/Swift 3.0/CoreDataTutorial/CoreDataTutorial/AppointmentViewController.swift
@@ -0,0 +1,110 @@
+//
+//  AppointmentViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Shridhar Mali on 2/2/17.
+//  Copyright © 2017 Shridhar Mali. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class AppointmentViewController: UIViewController {
+    
+    @IBOutlet weak var confirmBtn: UIButton!
+    @IBOutlet weak var nameOfPatient: UITextField!
+    @IBOutlet weak var ageOfPatient: UITextField!
+    @IBOutlet weak var addressOfPatient: UITextField!
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !appDelegate.isAddPatient {
+            self.confirmBtn.setTitle("Update", for: .normal)
+            nameOfPatient.text = appDelegate.selectedPatient.value(forKey: "name") as! String?
+            if let age = appDelegate.selectedPatient.value(forKey: "age") as! Int? {
+                ageOfPatient.text = String(describing:age)
+            }
+            addressOfPatient.text = appDelegate.selectedPatient.value(forKey: "address") as! String?
+        } else {
+            self.confirmBtn.setTitle("Confirm", for: .normal)
+        }
+    }
+
+    func getContext () -> NSManagedObjectContext {
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func confirmAppointment(_ sender: Any) {
+        
+        if nameOfPatient.text?.characters.count == 0 {
+            
+        } else if ageOfPatient.text?.characters.count == 0 {
+            
+        } else if addressOfPatient.text?.characters.count == 0 {
+            
+        } else {
+            let name: String! = nameOfPatient.text
+            let age: Int! = Int(ageOfPatient.text!)
+            let address: String! = addressOfPatient.text
+            print(name)
+            print(age)
+            print(address)
+            
+            saveDataInDatabase(name: name, age: age, address: address)
+        }
+        
+    }
+    
+    func saveDataInDatabase(name: String, age: Int, address: String) {
+        let context = getContext()
+        //retrieve the entity that we just created
+        let entity =  NSEntityDescription.entity(forEntityName: "Student", in: context)
+        let manageObjectStudent = NSManagedObject(entity: entity!, insertInto: context)
+        if !appDelegate.isAddPatient {
+            // Update
+            print("Update here")
+            
+        } else {
+            // Add
+            manageObjectStudent.setValue(name, forKey: "name")
+            manageObjectStudent.setValue(age, forKey: "age")
+            manageObjectStudent.setValue(address, forKey: "address")
+            
+        }
+        
+        //save the object
+        do {
+            try context.save()
+            print("saved!")
+            self.dismiss(animated: true, completion: nil)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        } catch {
+            
+        }
+    }
+}
+
+
+extension AppointmentViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+}
---
+++ iOS/Swift 3.0/CoreDataTutorial/CoreDataTutorial/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Shridhar Mali on 2/1/17.
+//  Copyright © 2017 Shridhar Mali. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tblPatient: UITableView!
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var patientData = [NSManagedObject]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Patients"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(self.addAppointment))
+        readDatabase()
+    }
+    
+    func addAppointment() {
+        patientData.removeAll()
+        appDelegate.isAddPatient = true
+        self.performSegue(withIdentifier: "addAppointment", sender: self)
+    }
+    
+    func readDatabase() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        do {
+            patientData.removeAll()
+            let data = try getContext().fetch(fetchRequest)
+            print(data.count)
+            for patient in data as! [NSManagedObject] {
+                print(patient.value(forKey: "name")!)
+                print(patient.value(forKey: "age")!)
+                print(patient.value(forKey: "address")!)
+                patientData.append(patient)
+            }
+            tblPatient.reloadData()
+        } catch {
+            
+        }
+    }
+
+    func getContext () -> NSManagedObjectContext {
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return patientData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if patientData.count != 0 {
+            cell.textLabel?.text = patientData[indexPath.row].value(forKey: "name") as! String?
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let object = patientData[indexPath.row] 
+            let context = getContext()
+            context.delete(object)
+            patientData.remove(at: indexPath.row)
+            do {
+                try context.save()
+                print("Object deleted from DB")
+                tblPatient.reloadData()
+            } catch let error as NSError {
+                print(error)
+            } catch {
+                
+            }
+        }
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        appDelegate.isAddPatient = false
+        appDelegate.selectedPatient = patientData[indexPath.row]
+        self.performSegue(withIdentifier: "addAppointment", sender: self)
+    }
+}
+
+
